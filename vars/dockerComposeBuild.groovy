4a2772a571df66977d73c6d7405445a1d00c7aa1
Nitin Sharma <nitinsharma1059@gmail.com>
2026-02-13 11:42:40 +0530

    Change docker compose command to use sh

--- vars/dockerComposeBuild.groovy
+++ vars/dockerComposeBuild.groovy
@@ -1,5 +1,5 @@
 def call(){
 echo 'building docker container'
-  docker compose down && docker compose up --build
+ sh docker compose down && docker compose up --build
 echo 'container build successfully'
 }
