d07e9036790f6c9c50cf686b634d879dbb45f174
Nitin Sharma <nitinsharma1059@gmail.com>
2026-03-03 17:40:20 +0530

    Notify about container removal in Docker build process
    Added echo statement to notify about container removal before building.

--- vars/dockerComposeBuild.groovy
+++ vars/dockerComposeBuild.groovy
@@ -2,6 +2,7 @@ def call() {
     echo 'Building Docker container...'
 
     sh '''
+       echo 'removing container'
         docker compose down || true
         docker compose up --build -d
     '''
